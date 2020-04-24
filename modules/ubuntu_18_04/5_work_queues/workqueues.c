edc1992b5234823e3dd90dec74ffb384288fcb26
Steve Evers <embedifying@gmail.com>
2020-04-23 18:36:26 -0700

    add template code

---
+++ modules/ubuntu_18_04/5_work_queues/workqueues.c
@@ -0,0 +1,28 @@
+// #include <linux/slab.h>
+#include "../utils/util.h"
+
+/* references
+*/
+
+void workqueues_init(void)
+{
+    PR_INFO("entry");
+}
+
+
+void workqueues_exit(void)
+{
+    PR_INFO("entry");
+}
+
+
+void workqueues_open(void)
+{
+    PR_INFO("entry");
+}
+
+
+void workqueues_close(void)
+{
+    PR_INFO("entry");
+}
---
+++ modules/ubuntu_18_04/5_work_queues/workqueues.h
@@ -0,0 +1,6 @@
+#pragma once
+
+void workqueues_init(void);
+void workqueues_exit(void);
+void workqueues_open(void);
+void workqueues_close(void);
