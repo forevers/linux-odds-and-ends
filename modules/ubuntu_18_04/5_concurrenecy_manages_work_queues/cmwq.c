cd974f55a6bc1e227ba7d1275b466e6cb5feb86b
Steve Evers <embedifying@gmail.com>
2020-04-25 18:11:14 -0700

    add concurrency managed work queue

---
+++ modules/ubuntu_18_04/5_concurrenecy_manages_work_queues/cmwq.c
@@ -0,0 +1,131 @@
+#include <linux/cpumask.h>
+#include <linux/getcpu.h>
+#include <linux/jiffies.h>
+#include <linux/list.h>
+#include <linux/sched.h>
+#include <linux/slab.h>
+#include <linux/smp.h> 
+#include <linux/workqueue.h>
+
+#include <linux/random.h>
+
+#include "../utils/util.h"
+
+/* references
+https://www.kernel.org/doc/html/v4.14/core-api/workqueue.html
+*/
+
+/* workqueue */
+static struct workqueue_struct* workqueue_;
+
+/* work struct */
+struct Data {
+    struct work_struct work;
+    int value;
+} *data_;
+
+/* delayed work struct */
+struct DataDelayed {
+    struct delayed_work work;
+    int value;
+    /* list maintained to cancel work tasks */
+    struct list_head list;
+} *data_delayed_2_, *data_delayed_4_, *data_delayed_6_;
+
+/* work list to manage queued work items*/
+LIST_HEAD(work_list_);
+
+
+/* non-delayed work item processing */
+static void do_work(struct work_struct* work_arg)
+{
+    PR_INFO("entry");
+
+    {
+        /* get container to oobtain data payload */
+        struct Data* data_container = container_of(work_arg, struct Data, work);
+        PR_INFO("value = %d", data_container->value);
+        /* delelete Data created in init */
+        kfree(data_container);
+    }
+}
+
+/* delayed work item processing */
+static void do_work_delayed(struct work_struct* work_arg)
+{
+    PR_INFO("entry");
+
+    {
+        int cpu = smp_processor_id();
+        /* get container to oobtain data payload */
+        struct delayed_work* delayed_work = to_delayed_work(work_arg);
+        struct DataDelayed* data_container = container_of(delayed_work, struct DataDelayed, work);
+        PR_INFO("value = %d, cpu = %d", data_container->value, cpu);
+        /* remove work item from list */
+        list_del(&data_container->list);
+        /* delelete Data created in init */
+        kfree(data_container);
+    }
+}
+
+/* module init */
+void cmwq_init(void)
+{
+    int i;
+    int num_cores = num_online_cpus();
+    int max_active = prandom_u32_max(16);
+
+    PR_INFO("entry");
+    PR_INFO("%d cpus", num_cores);
+    PR_INFO("create cmwq with max_active threads: %d", max_active);
+
+    workqueue_ = alloc_workqueue("cmwq_work_queue", WQ_MEM_RECLAIM, max_active);
+
+    data_  = kmalloc(sizeof(struct Data), GFP_KERNEL);
+    data_->value = 0;
+    INIT_WORK(&data_->work, do_work);
+    queue_work(workqueue_, &data_->work);
+
+    for (i = 0; i < num_cores; i++) {
+        struct DataDelayed* data_delayed = kmalloc(sizeof(struct DataDelayed), GFP_KERNEL);
+        data_delayed->value = i;
+        INIT_DELAYED_WORK(&data_delayed->work, do_work_delayed);
+        queue_delayed_work_on(i, workqueue_, &data_delayed->work, msecs_to_jiffies(i*1000));
+
+        list_add(&data_delayed->list, &work_list_);
+    }
+}
+
+/* module exit */
+void cmwq_exit(void)
+{
+    struct list_head* safe_iter_temp;
+    struct list_head* safe_iter_pos;
+    struct DataDelayed* data_delayed_node;
+
+    PR_INFO("entry");
+
+    list_for_each_safe(safe_iter_pos, safe_iter_temp, &work_list_) {
+        data_delayed_node = list_entry(safe_iter_pos, struct DataDelayed, list);
+        cancel_delayed_work(&data_delayed_node->work);
+        list_del(&data_delayed_node->list);
+        PR_INFO("delist and delete work item %d ", data_delayed_node->value);
+        kfree(data_delayed_node);
+    }
+
+    PR_INFO("flush and destroy workqueue");
+    flush_workqueue(workqueue_);
+    destroy_workqueue(workqueue_);
+}
+
+
+void cmwq_open(void)
+{
+    PR_INFO("entry");
+}
+
+
+void cmwq_close(void)
+{
+    PR_INFO("entry");
+}
---
+++ modules/ubuntu_18_04/5_concurrenecy_manages_work_queues/ess_canonical_module.c
@@ -0,0 +1,139 @@
+/* 
+    build syntax :
+        cross :
+            $ make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- -C <path to cross compiled kernel>/linux M=$(pwd) modules
+        native :
+            $ make -C /lib/modules/$(uname -r)/build M=$(pwd) modules
+
+    // target module log realtime monitoring
+    $ dmesg -wH
+
+    load syntax :
+        $ sudo insmod ess_cmwq.ko
+        // verify rw access
+        $ ls -l /dev/ess-device-name
+            crw-rw-rw- 1 root root 240, 0 Sep 23 19:57 /dev/ess-device-name
+        $ sudo rmmod
+
+    references :
+        https://www.kernel.org/doc/html/v4.19/
+*/
+
+#include <linux/cdev.h>
+#include <linux/device.h>
+#include <linux/fs.h>
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+
+#include "ess_canonical_module.h"
+#include "../utils/util.h"
+
+MODULE_AUTHOR("Developer Name <developer_email>");
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("a canonical driver template");
+MODULE_VERSION("0.1");
+
+#include "cmwq.h"
+
+/* driver parameters */
+#define NUM_DEVICES             1
+#define FIRST_REQUESTED_MINOR   0
+#define ESS_DEVICE_NAME         "ess-device-name"
+static struct class* ess_class;
+static struct cdev ess_cdev;
+static dev_t ess_dev_no;
+
+
+int ess_open(struct inode *i, struct file *f)
+{
+    PR_INFO("entry");
+    cmwq_open();
+    return 0;
+}
+
+
+int ess_close(struct inode *i, struct file *f)
+{
+    PR_INFO("entry()");
+    cmwq_close();
+    return 0;
+}
+
+
+// see include/linux/fs.h for full fops description
+static struct file_operations ess_fops =
+{
+  .owner = THIS_MODULE,
+  .open = ess_open,
+  .release = ess_close,
+};
+
+
+/* module load 
+    for built-ins this code is placed in a mem section which is freed after init is complete
+*/
+static int __init
+canonical_init(void)
+{
+    int ret;
+    int major;
+
+    PR_INFO("entry");
+
+    cmwq_init();
+
+    if (0 > (ret = alloc_chrdev_region(&ess_dev_no, FIRST_REQUESTED_MINOR, NUM_DEVICES, "ess_region"))) {
+        PR_ERR("alloc_chrdev_region() failure");
+        return ret;
+    }
+
+    /* request /sys/class entry */
+    if (NULL == (ess_class = class_create(THIS_MODULE, "ess_class"))) {
+        PR_ERR("class_create() failure");
+        return -1;
+    }
+
+    /* device node */
+    if (device_create(ess_class, NULL, ess_dev_no, NULL, ESS_DEVICE_NAME) == NULL) {
+        class_destroy(ess_class);
+        unregister_chrdev_region(ess_dev_no, 1);
+        return -1;
+    }
+
+    cdev_init(&ess_cdev, &ess_fops);
+    ess_cdev.owner = THIS_MODULE;
+
+    if (cdev_add(&ess_cdev, ess_dev_no, 1) == -1) {
+        device_destroy(ess_class, ess_dev_no);
+        class_destroy(ess_class);
+        unregister_chrdev_region(ess_dev_no, 1);
+        return -1;
+    }
+
+    major = MAJOR(ess_dev_no);
+    PR_INFO("major number : %d", major);
+
+    return 0;
+}
+
+
+/* module unload for non-built ins */
+static void __exit
+canonical_exit(void)
+{
+    PR_INFO("entry");
+
+    cmwq_exit();
+
+    /* driver teardown */
+    PR_INFO("release driver resources");
+    device_destroy(ess_class, ess_dev_no);
+    class_destroy(ess_class);
+    unregister_chrdev_region(ess_dev_no, NUM_DEVICES);
+
+}
+
+
+module_init(canonical_init);
+module_exit(canonical_exit);
